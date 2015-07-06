11d0a4031ce8fb9f7eea96c1784d2f0dfbcf3fae
Felice Pantaleo <felicepantaleo@users.noreply.github.com>
2015-07-06 17:56:11 +0200

    Update kernel.cu
    skeleton

--- kernel.cu
+++ kernel.cu
@@ -1,1 +1,6 @@
 
+    // future from a packaged_task
+    std::packaged_task<int()> task([](){ return 42; });
+    std::future<int> futureAnswerToLifeUniverseAndEverything = task.get_future();  
+    std::thread(std::move(task)).detach(); 
+    std::cout << futureAnswerToLifeUniverseAndEverything.get() << std::endl;
